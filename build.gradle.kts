6e783fed1b1082f25b7082626e0d22596decdccb
burt <yongtaobai@qq.com>
2023-08-04 13:20:53 +0800

    chore(plugin-template): Merged `JetBrains/intellij-platform-plugin-template` Tag `v1.6.1`

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,10 +89,10 @@ tasks {
         changeNotes = properties("pluginVersion").map { pluginVersion ->
             with(changelog) {
                 renderItem(
-                        (getOrNull(pluginVersion) ?: getUnreleased())
-                                .withHeader(false)
-                                .withEmptySections(false),
-                        Changelog.OutputType.HTML,
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
                 )
             }
         }
